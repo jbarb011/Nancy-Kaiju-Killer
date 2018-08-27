53baa5e4abf0fa70321399fa2b946fe4bf756ee2
Jerome Barbero <jbarb011@ucr.edu>
2018-08-27 16:52:21 -0700

    edited jumping. no longer charge jump, but tap instead

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,26 +4,29 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
-    public bool facingRight = true;             //Sprite Direction
+    public bool facingRight = true;             // Sprite Direction
 
-    public int playerSpeed = 10;                //X-axis speed
-    public float moveX;                         //X-axis movement
+    public int playerSpeed = 10;                // X-axis speed
+    public float moveX;                         // X-axis movement
 
-    public float playerJumpMin = 500f;          //Minimum Ground Jump Speed
-    public float playerJumpMax = 1500f;         //Maximum Ground Jump Speed
-    public float TimeCharge = 0f;               //Timer for Charging Jump
-    public float ChargeMultiplier = 3000f;      //How fast Charging Jump accumulates
+    public float groundJumpSpeed = 1500f;       // Speed in which Player Jumps
 
-    public float AirJumpSpeed = 100f;           //Jump for when in air
-    public int NumAirJumps = 2;              // Number of air jumps you can make in game
-    public int AirJumps;                        // Number of air jumps currently available
+    public float airJumpSpeed = 1000f;          // Jump for when in air
+    public int numAirJumps = 2;                 // Number of air jumps you can make in game
+    public int airJumps;                        // Number of air jumps currently available
     public bool inAir;                          // Check if not on Ground
 
-    Rigidbody2D rb;
+    Rigidbody2D rb;                             // Rigidbody for Player
 
     void Awake ()
     {
+        //Get Rigidbody Component
         rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Start()
+    {
+        airJumps = numAirJumps;
     }
 
 	// Update is called once per frame
@@ -31,27 +34,7 @@ public class PlayerMovement : MonoBehaviour {
 
         //INPUT
         moveX = Input.GetAxis("Horizontal");
-
-
-        //JUMPING
-        if (!inAir)
-        {
-            //CALCULATE TIME OF INPUT JUMP
-            if (Input.GetButton("Jump")) TimeCharge += Time.deltaTime;
-            //WHEN SPACE IS LET GO, JUMP
-            if (Input.GetButtonUp("Jump")) ChargeJump();
-        }
-        else
-        {
-            //If In Air only do NumAirJumps amount of times
-            if (Input.GetButtonDown("Jump") && AirJumps > 0)
-            {
-                rb.velocity = new Vector2(rb.velocity.x, 0);
-                rb.AddForce(Vector2.up * AirJumpSpeed);
-                AirJumps--;
-                Debug.Log(AirJumps);
-            }
-        }
+        if (Input.GetButtonDown("Jump")) Jump();
 
 
 
@@ -61,54 +44,59 @@ public class PlayerMovement : MonoBehaviour {
 
 
 
-        //PHYSICS
+        //LEFT AND RIGHT MOVEMENT
         rb.velocity = new Vector2(moveX * playerSpeed, rb.velocity.y);
-        Debug.Log(moveX * playerSpeed);
 	}
 
-    void ChargeJump()
+    void Jump()
     {
-        //CALCULATE JUMP CHARGE
-        float JumpCharge = (TimeCharge*ChargeMultiplier) + playerJumpMin;
 
-
-
-        //IF JUMP CHARGE IS LESS THAN MAX JUMP DO JUMP CHARGE
-        //ELSE DO MAX JUMP
-        if (JumpCharge < playerJumpMax)
+        // Speed of jump depends on whether or not you are in air or not
+        if (inAir && airJumps > 0)
         {
-            rb.AddForce(Vector2.up * JumpCharge);
-        }
-        else
-        {
-            rb.AddForce(Vector2.up * playerJumpMax);
-        }
+            rb.velocity = new Vector2(rb.velocity.x, 0);
+            rb.AddForce(Vector2.up * airJumpSpeed);
 
-        //RESTART TIMECHARGE
-        TimeCharge = 0f;
+            // Subtract air jumps
+            airJumps--;
+        }
+        else if (!inAir)
+        {
+            rb.AddForce(Vector2.up * groundJumpSpeed);
+        }
+        
     }
 
     void FlipPlayer()
     {
-        //FLIP SPRITE OF CHARACTER
+        // Flip sprite of character depending where they are moving
         facingRight = !facingRight; 
         Vector3 curr_scale = gameObject.GetComponent<Transform>().localScale;
         gameObject.GetComponent<Transform>().localScale = new Vector3(-curr_scale.x, curr_scale.y, curr_scale.z);
     }
 
+
+    // Check if on ground or air
     void OnTriggerEnter2D(Collider2D col)
     {
         if (col.gameObject.tag == "Ground")
         {
-            inAir = false;
-            AirJumps = NumAirJumps;
+            //Reset airjumps if landing
+            if (inAir)
+            {
+                airJumps = numAirJumps;
+                inAir = false;
+            }
         }
     }
     void OnTriggerExit2D(Collider2D col)
     {
         if (col.gameObject.tag == "Ground")
         {
-            inAir = true;
+            if (!inAir)
+            {
+                inAir = true;
+            }
         }
     }
 }
